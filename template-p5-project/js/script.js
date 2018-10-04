e065889fdaa0781b7a2d4c80a112dc4abacf1d66
Carlos Giron-Bran <carlos.gbran@gmail.com>
2018-10-04 11:21:10 -0400

    gen

--- template-p5-project/js/script.js
+++ template-p5-project/js/script.js
@@ -1,22 +1,39 @@
-var x;
-var y;
-var tx;
-var ty;
+
+var pointsMax=100;
+var points;
+var tx=0;
+var ty=0;
+var x=0;
+
 
 function setup() {
-  createCanvas(500,500);
-  tx = random(0,1000);
-  ty = random(0,1000);
+  createCanvas(1000,500);
+  background(51);
+  noStroke();
+  fill(random(255),random(255),random(255),175);
+
 }
 
 function draw() {
-  background(255);
+if (x<width) {
+  drawSq();
+  console.log('drawing');
+  }
+else {
+  resetAtt();
+  console.log('and another one!');
+}
 
-  x = width * noise(tx);
-  y = height * noise(ty);
 
-  tx += 0.01;
-  ty += 0.01;
-
-  ellipse(x,y,10,10);
+}
+function drawSq(){
+  var y = height*noise(ty);
+  ellipse(x,y,5,5);
+  x+=0.75;
+  ty+= 0.01;
+}
+function resetAtt(){
+  x=0;
+  noStroke();
+  fill(random(255),random(255),random(255),175);
 }
