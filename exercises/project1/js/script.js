ffa4c735ba1cdaa9f6c3493f8778af78e0d7dff6
Carlos Giron-Bran <carlos.gbran@gmail.com>
2018-10-14 15:19:11 -0400

    Update exercises/project1/js/script.js

---
+++ exercises/project1/js/script.js
@@ -0,0 +1,381 @@
+/******************************************************
+
+Game - Chaser
+Pippin Barr
+
+A simple game of cat and mouse.
+
+Physics-based movement, keyboard controls, health/stamina,
+sprinting, random movement, screen wrap.
+
+
+1.DONE Modify the way the prey moves to use Perlin noise and the noise() function instead of the random() function it uses now.
+(You'll need to get rid of the conditional that chooses when the prey changes direction, it should only use noise()).
+
+2.DONE Add the ability to "sprint" for the player when they hold down the shift key (using keyDown() in the handleInput() function for this is probably a good idea).
+Make them lose health faster when they are sprinting. Don't forget to reset to the player's normal speed when they stop sprinting.
+
+3.Add variables and conditionals as needed to make the game more interesting to play by varying the player and prey's size, speed, visibility,
+and/or anything else you can think of during play (maybe the prey get harder to see as the player gets more successful, maybe the player gets bigger the more it eats, etc.)
+ - try to have a reason why each thing happens.
+
+4.Tune the values of the game's variables to make the gameplay more interesting to play (is the prey too slow? Should it move more erratically?
+Should the player be faster? Slower? Etc.)
+
+5.Change the visuals of the game and add sounds so that the game is no longer abstract but conveys an idea through both its gameplay and through its aesthetics
+(maybe it could be about politics, or relationships, or cooking, or playing chess, or anything else!).
+The new visuals and sounds should match the gameplay you created in step two
+
+  *Make sure you use functions to better organise your code!
+  *Don't limit yourself to the above five challenges, if you've got the time, make the game even better!
+
+******************************************************/
+
+// Track whether the game is over
+var gameOver = false;
+var gameOverTired = false;
+
+// Player position, size, velocity
+var playerX;
+var playerY;
+var playerRadius = 25;
+var playerVX = 0;
+var playerVY = 0;
+var playerMaxSpeed = 5;
+
+// Player health
+var playerHealth;
+var playerMaxHealth = 255;
+// Player fill color
+var playerFill = 50;
+
+// Prey position, size, velocity
+var preyX;
+var preyY;
+var preyRadius = 25;
+var preyVX;
+var preyVY;
+var preyMaxSpeed = 7;
+// Prey health
+var preyHealth;
+var preyMaxHealth = 100;
+// Prey fill color
+var preyFill = 200;
+
+// Amount of health obtained per frame of "eating" the prey
+var eatHealth = 10;
+// Number of prey eaten during the game
+var preyEaten = 0;
+
+// setup()
+//
+// Sets up the basic elements of the game
+function setup() {
+  createCanvas(500,500);
+
+  noStroke();
+
+  setupPrey();
+  setupPlayer();
+}
+
+// setupPrey()
+//
+// Initialises prey's position, velocity, and health
+function setupPrey() {
+  preyX = width/5;
+  preyY = height/2;
+  preyVX = -preyMaxSpeed;
+  preyVY = preyMaxSpeed;
+  preyHealth = preyMaxHealth;
+}
+
+// setupPlayer()
+//
+// Initialises player position and health
+function setupPlayer() {
+  playerX = 4*width/5;
+  playerY = height/2;
+  playerHealth = playerMaxHealth;
+}
+
+// draw()
+//
+// While the game is active, checks input
+// updates positions of prey and player,
+// checks health (dying), checks eating (overlaps)
+// displays the two agents.
+// When the game is over, shows the game over screen.
+function draw() {
+  background(100,100,200);
+
+  if (!gameOver) {
+    handleInput();
+
+    movePlayer();
+    movePrey();
+
+    updateHealth();
+    checkEating();
+
+    drawPrey();
+    drawPlayer();
+  }
+  else {
+    showGameOver();
+    noLoop();
+
+  }
+  if(!gameOverTired){
+    handleInput();
+
+    movePlayer();
+    movePrey();
+
+    updateHealth();
+    checkEating();
+
+    drawPrey();
+    drawPlayer();
+  }
+  else{
+    background(0);
+    showGameOverTired();
+    noLoop();
+
+
+  }
+}
+
+// handleInput()
+//
+// Checks arrow keys and adjusts player velocity accordingly
+function handleInput() {
+  // Check for horizontal movement
+  if (keyIsDown(LEFT_ARROW)) {
+    playerVX = -playerMaxSpeed;
+  }
+  else if (keyIsDown(RIGHT_ARROW)) {
+    playerVX = playerMaxSpeed;
+  }
+  else {
+    playerVX = 0;
+  }
+
+  // Check for vertical movement
+  if (keyIsDown(UP_ARROW)) {
+    playerVY = -playerMaxSpeed;
+  }
+  else if (keyIsDown(DOWN_ARROW)) {
+    playerVY = playerMaxSpeed;
+  }
+  else {
+    playerVY = 0;
+  }
+
+  //Check for speed increase if SHIFT key is pressed.
+  //Nested the original inputHandle
+
+  if (keyIsDown(SHIFT)){
+    //Boost also affect health by 0.5 more loss when boost.
+    playerHealth = (constrain(playerHealth - 0.5,0,playerMaxHealth))-0.2;
+    var playerBoost = playerMaxSpeed+(playerMaxSpeed/2);
+
+    if (keyIsDown(LEFT_ARROW)) {
+      playerVX = -playerBoost;
+    }
+    else if (keyIsDown(RIGHT_ARROW)) {
+      playerVX = playerBoost;
+    }
+    else {
+      playerVX = 0;
+    }
+
+    if (keyIsDown(UP_ARROW)) {
+      playerVY = -playerBoost;
+    }
+    else if (keyIsDown(DOWN_ARROW)) {
+      playerVY = playerBoost;
+    }
+    else {
+      playerVY = 0;
+    }
+
+  }
+
+}
+
+// movePlayer()
+//
+// Updates player position based on velocity,
+// wraps around the edges.
+function movePlayer() {
+  // Update position
+  playerX += playerVX;
+  playerY += playerVY;
+
+  // Wrap when player goes off the canvas
+  if (playerX < 0) {
+    playerX += width;
+  }
+  else if (playerX > width) {
+    playerX -= width;
+  }
+
+  if (playerY < 0) {
+    playerY += height;
+  }
+  else if (playerY > height) {
+    playerY -= height;
+  }
+  //try to create a sluggish death! for now it work if you dont die from desappearing first.(don't know what to do yet!)
+  if(playerMaxSpeed<=2){
+    playerMaxSpeed+=-0.001;
+  }
+  if(playerMaxSpeed<=0){
+    gameOverTired=true;
+  }
+}
+
+// updateHealth()
+//
+// Reduce the player's health (every frame)
+// Check if the player is dead
+function updateHealth() {
+  // Reduce player health, constrain to reasonable range
+  playerHealth = constrain(playerHealth - 0.5,0,playerMaxHealth);
+
+  // Check if the player is dead
+  if (playerHealth === 0) {
+    // If so, the game is over
+    gameOver = true;
+  }
+}
+
+// checkEating()
+//
+// Check if the player overlaps the prey and updates health of both
+function checkEating() {
+  // Get distance of player to prey
+  var d = dist(playerX,playerY,preyX,preyY);
+  // Check if it's an overlap
+  if (d < playerRadius + preyRadius) {
+    // Increase the player health
+    playerHealth = constrain(playerHealth + eatHealth,0,playerMaxHealth);
+    // Reduce the prey health
+    preyHealth = constrain(preyHealth - eatHealth,0,preyMaxHealth);
+
+    // Check if the prey died
+    if (preyHealth === 0) {
+      // Move the "new" prey to a random position
+      preyX = random(0,width);
+      preyY = random(0,height);
+      // Give it full health
+      preyHealth = preyMaxHealth;
+      // Track how many prey were eaten
+      preyEaten++;
+
+      //everytime the prey is eaten, the prey and the player slow down at random intervals. But prey never stops completly. Player will die before.
+    if(preyEaten++){
+      playerMaxSpeed+=-random(0.05,0.5);
+      preyMaxSpeed+=-random(0.05,1);
+      }
+      if(playerMaxSpeed==0){
+        playerBoost=0;
+      }
+      if (playerMaxSpeed<=0){
+      gameOverTired = true;
+
+      if(preyMaxSpeed<=1){
+        preyMaxSpeed+=random(0.1,1);
+      }
+      if (preyMaxSpeed>5) {
+        preyMaxSpeed+=-random(0.1,1)
+      }
+
+    }
+
+
+    }
+  }
+}
+
+// movePrey()
+//
+// Moves the prey based on random velocity changes
+function movePrey() {
+  // Change the prey's velocity at random intervals
+  // random() will be < 0.05 5% of the time, so the prey
+  // will change direction on 5% of frames
+  if (random() < 0.05) {
+    // Set velocity based on random values to get a new direction
+    // and speed of movement
+    // Use map() to convert from the 0-1 range of the noise() function
+    // to the appropriate range of velocities for the prey
+
+    // variables to calculate noise  prey speed. X Y different/separate values!
+    var nRspeedX=width*random(map(preyMaxSpeed,0,4,0,1));
+    var nRspeedY=height*random(map(preyMaxSpeed,0,4,0,1));
+
+    preyVX = map(noise(nRspeedX),0,1,-preyMaxSpeed,preyMaxSpeed);
+    preyVY = map(noise(nRspeedY),0,1,-preyMaxSpeed,preyMaxSpeed);
+  }
+
+  // Update prey position based on velocity
+  preyX += preyVX;
+  preyY += preyVY;
+
+  // Screen wrapping
+  if (preyX < 0) {
+    preyX += width;
+  }
+  else if (preyX > width) {
+    preyX -= width;
+  }
+
+  if (preyY < 0) {
+    preyY += height;
+  }
+  else if (preyY > height) {
+    preyY -= height;
+  }
+}
+
+// drawPrey()
+//
+// Draw the prey as an ellipse with alpha based on health
+function drawPrey() {
+  fill(preyFill,preyHealth);
+  ellipse(preyX,preyY,preyRadius*2);
+}
+
+// drawPlayer()
+//
+// Draw the player as an ellipse with alpha based on health
+function drawPlayer() {
+  fill(playerFill,playerHealth);
+  ellipse(playerX,playerY,playerRadius*2);
+}
+
+// showGameOver()
+//
+// Display text about the game being over!
+function showGameOver() {
+  textSize(32);
+  textAlign(CENTER,CENTER);
+  fill(0);
+  var gameOverText = "GAME OVER\n";
+  gameOverText += "You ate " + preyEaten + " prey\n";
+  gameOverText += "before you died."
+  text(gameOverText,width/2,height/2);
+}
+function showGameOverTired(){
+  textSize(32);
+  textAlign(CENTER,CENTER);
+  fill(255);
+  var gameOverText = "YOU'RE TIRED\n";
+  gameOverText += "You ate " + preyEaten + " prey\n";
+  gameOverText += "before you died of exhaustion."
+  text(gameOverText,width/2,height/2);
+
+}
