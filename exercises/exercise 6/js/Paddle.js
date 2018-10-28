ba930f6ebad43a6965d62bb80c5b80bb8a68a1d9
Carlos Giron-Bran <carlos.gbran@gmail.com>
2018-10-28 15:02:09 -0400

    Ex6: starded

---
+++ exercises/exercise 6/js/Paddle.js
@@ -0,0 +1,58 @@
+// Paddle
+//
+// A class that defines how a paddle behaves, including the ability
+// to specify the input keys to move it up and down
+/////// FIXED
+//Paddle constructor
+//Sets the properties with the provided arguments or defaults
+////// FIXED
+function Paddle(x,y,w,h,speed,downKey,upKey) {
+  this.x = x;
+  this.y = y;
+  this.xv = 0;
+  this.yv = 0;
+  this.w = w;
+  this.h = h;
+////// FIXED
+  this.speed = speed;
+  this.downKey = downKey;
+  this.upKey = upKey;
+}
+
+// handleInput()
+//
+// Check if the up or down keys are pressed and update velocity
+// appropriately
+/////// FIXED
+Paddle.prototype.handleInput = function() {
+////// FIXED
+  if (keyIsDown(this.upKey)) {
+    this.vy = -this.speed;
+  }
+////// FIXED
+  else if (keyIsDown(this.downKey)) {
+    this.vy = this.speed;
+  } else {
+    this.vy=0;
+  }
+}
+
+// update()
+// Update y position based on velocity
+// Constrain the resulting position to be within the canvas
+Paddle.prototype.update = function() {
+  this.y += this.vy;
+////// FIXED
+  this.y = constrain(this.y,0,height-this.h);
+}
+
+// display()
+//
+// Draw the paddle as a rectangle on the screen
+////// FIXED
+Paddle.prototype.display = function() {
+////// FIXED
+  fill(255);
+////// FIXED
+  rect(this.x,this.y,this.w,this.h);
+}
---
+++ exercises/exercise 6/js/script.js
@@ -0,0 +1,63 @@
+// Broken Basic OO Pong
+// by Pippin Barr
+//
+// A broken primitive implementation of Pong with no scoring system
+// just the ability to play the game with the keyboard.
+//
+// Arrow keys control the right hand paddle, W and S control
+// the left hand paddle.
+//
+// Written with JavaScript OOP.
+
+// Variable to contain the objects representing our ball and paddles
+////// FIXED
+var ball;
+var leftPaddle;
+var rightPaddle;
+
+// setup()
+//
+// Creates the ball and paddles
+function setup() {
+  ////// FIXED
+  createCanvas(640,480);
+  noStroke();
+  // Create a ball
+  ball = new Ball(width/2,height/2,5,5,10,10);
+  // Create the right paddle with UP and DOWN as controls
+////// FIXED
+  rightPaddle = new Paddle(width-10,height/2,10,60,10,DOWN_ARROW,UP_ARROW);
+  // Create the left paddle with W and S as controls
+  // Keycodes 83 and 87 are W and S respectively
+////// FIXED
+  leftPaddle = new Paddle(0,height/2,10,60,10,83,87);
+////// FIXED
+}
+// draw()
+//
+// Handles input, updates all the elements, checks for collisions
+// and displays everything.
+function draw() {
+  background(0);
+
+  leftPaddle.handleInput();
+  rightPaddle.handleInput();
+
+////// FIXED
+  ball.update();
+  leftPaddle.update();
+  rightPaddle.update();
+
+////// FIXED
+  if (ball.isOffScreen()) {
+    ball.reset();
+  }
+
+  ball.handleCollision(leftPaddle);
+  ball.handleCollision(rightPaddle);
+
+  ball.display();
+  leftPaddle.display();
+////// FIXED
+  rightPaddle.display();
+}
